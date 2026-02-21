a1c179d6ce335ebe56669d5336444c966d428345
Hugo Norte <hugonorte@gmail.com>
2026-02-21 14:24:40 -0300

    Adding page title component

---
+++ app/components/PageTitle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+    title: String,
+})
+</script>
+
+<template>
+    <div class="pageTitle">
+        <h1>{{ props.title }}</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.pageTitle {
+    text-align: left;
+    padding-left: 2rem;
+    width: 100%;
+    height: fit-content;
+
+    h1 {
+        margin: 0;
+        font-size: 1.5rem;
+        color: #1c5330;
+    }
+}
+</style>
