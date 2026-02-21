6a98cba99b6fd51d4eaef8481f80422038633532
Hugo Norte <hugonorte@gmail.com>
2026-02-21 14:25:14 -0300

    Refactoring semanticly

--- app/layouts/home.vue
+++ app/layouts/home.vue
@@ -15,16 +15,18 @@ useHead({
 </script>
 
 <template>
-  <main>
+  <body>
     <HeaderView />
-    <slot />
+    <main>
+      <slot />
+    </main>
     <FooterView />
-  </main>
+  </body>
 </template>
 
 <style scoped lang="scss">
-main {
-  min-height: 100vh;
+body {
+  height: 100vh;
   width: 100vw;
   display: flex;
   flex-direction: column;
