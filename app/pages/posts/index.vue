591e0dbe22d0ae84c5c10c5fab76b24b64a8c518
Hugo Norte <hugonorte@gmail.com>
2026-02-21 14:25:45 -0300

    refactoring

--- app/pages/posts/index.vue
+++ app/pages/posts/index.vue
@@ -5,24 +5,6 @@ definePageMeta({
 </script>
 
 <template>
-    <main>
-        <section class="posts_list_container">
-            <h1>Posts</h1>
-            <!-- Conteúdo da lista de posts virá aqui -->
-        </section>
-    </main>
+        <PageTitle title="Posts" />
+        <Posts />
 </template>
-
-<style scoped lang="scss">
-.posts_list_container {
-    padding: 2rem;
-    max-width: 1200px;
-    margin: 0 auto;
-    
-    h1 {
-        font-size: 2.5rem;
-        color: #034f04;
-        margin-bottom: 2rem;
-    }
-}
-</style>
