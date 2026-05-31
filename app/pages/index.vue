b4387b8cb25951dcd9ba7519926271276f71dc62
Hugo Norte <hugonorte@gmail.com>
2026-05-31 09:39:10 -0300

    chore: update page title with exclamation mark in index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@ definePageMeta({
 });
 
 useSeoMeta({
-  title: 'Abertamente - Saúde Física e Mental em um só Lugar',
+  title: 'Abertamente - Saúde Física e Mental em um só Lugar!',
   ogTitle: 'Abertamente - Saúde Física e Mental em um só Lugar',
   description: 'Encontre informações, dicas e artigos sobre saúde física e mental para uma vida mais equilibrada.',
   ogDescription: 'Encontre informações, dicas e artigos sobre saúde física e mental para uma vida mais equilibrada.',
